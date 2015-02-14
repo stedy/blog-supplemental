3e4021d2614ee84f6590678acc727ec40dc18dbb
Zach Stednick <zstednic@fhcrc.org>
2015-02-14 14:00:54 -0800

    fix missing data point

--- analysis/gas_calc.R
+++ analysis/gas_calc.R
@@ -13,10 +13,10 @@ final <- merge(costsummary, mpgsummary)
 ggplot(final, aes(x= meancost, y= meanMPG, label= Brand)) +
   geom_point(size=4) + 
   geom_text(aes(label= Brand), hjust= -0.2, vjust= -0.2, size=6) +
-  scale_x_continuous(limits = c(6,15)) +
+  scale_x_continuous(limits = c(6,10)) +
   ylab("mean MPG") + xlab("Mean cost") +
   theme(axis.text.x = element_text(face='bold', size=20),
         axis.text.y = element_text(face='bold', size=20),
         axis.title.y = element_text(face="bold", size=20, angle=90),
         axis.title.x = element_text(face="bold", size=20)
-  )
+)
